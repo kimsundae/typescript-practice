726b4ba3f48ea186d2fbdaacaacad9187108b8c4
kimgeunbae <naa159@naver.com>
2024-11-20 00:19:10 +0900

    타입 사용

--- 02.types/build/js/main.js
+++ 02.types/build/js/main.js
@@ -5,26 +5,28 @@ let number;
 let integer = 6;
 let float = 1.2345;
 let string;
-let firstName = 'Doe';
+let firstName = "Doe";
 // Array
 // 한가지 타입만 가지는 배열
-let names1 = ['John', 'Kim'];
-let names2 = ['John', 'Kim'];
-let array1 = ['John', 1, 2];
-let array2 = ['John', 1, 2];
-let someArray = ['John', 1, [], {}, false];
-let stringArray = ['A', 'B'];
+let names1 = ["John", "Kim"];
+let names2 = ["John", "Kim"];
+let array1 = ["John", 1, 2];
+let array2 = ["John", 1, 2];
+let someArray = ["John", 1, [], {}, false];
+let stringArray = ["A", "B"];
 let numberArray = [1, 2];
 // stringArray.push('C');
 // numberArray[0] = 3;
 let tuple1;
-tuple1 = ['a', 1];
-;
+tuple1 = ["a", 1];
 // tuple1 = ['a', 1, 2]
 // tuple1 = [1, 'a']
 let users;
-users = [[1, 'John'], [2, 'Doe']];
+users = [
+  [1, "John"],
+  [2, "Doe"],
+];
 let tuple2;
-tuple2 = ['a', 1];
+tuple2 = ["a", 1];
 tuple2.push(2);
 console.log(tuple2);
--- 02.types/src/main.ts
+++ 02.types/src/main.ts
@@ -35,3 +35,39 @@ let tuple2: [string, number]
 tuple2= ['a',1]
 tuple2.push(2)
 console.log(tuple2);
+
+let any: any = 'abc';
+any = 1;
+any= [];
+
+let unknown: unknown = false;
+// let string1: string = unknown;
+let string2: boolean = unknown as boolean;
+
+let obj: object = {};
+let arr: object = [];
+let nul: object = null;
+let date: object = new Date();
+
+const obj1: {id: number, title: string, description: string} = {
+  id: 1,
+  title: 'title1',
+  description: 'description1'
+}
+
+let union: (string | number)
+union = 'hi';
+union = 123;
+union = [];
+
+let func1: (arg1:number, arg2:number) => number;
+func1 = function(x,y){
+  return x * y
+}
+
+let func2: () => void;
+func2 = function (){
+  console.log('hi');
+}
+
+let number1: number = undefined
